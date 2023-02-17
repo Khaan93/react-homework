9a4124d6890cacebc0dbc6b4b943e4bf1624408c
Khaan93 <westtoraise@gmail.com>
2023-02-17 12:09:54 +0700

    17 lesson

--- src/components/menuList/index.js
+++ src/components/menuList/index.js
@@ -1,6 +1,9 @@
 const MenuList = ({ items }) => {
-    return items.map((item) => <div className="bg-yellow-500 border-0 "><div className="">{item.name}</div><br></br>
-        стоимость блюда: {item.price} руб.</div>);
+    return items.map((item) => (
+        <div className="bg-violet-500 border-0">
+            <h2>{item.name}</h2>
+            <div>Cтоимость блюда: {item.price} руб.</div>
+        </div>));
 };
 
 export default MenuList;
