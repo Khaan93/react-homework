8198643eab2ed580dc2c153bde289d578ec52e51
Khaan93 <westtoraise@gmail.com>
2023-02-17 10:57:28 +0700

    17 lesson

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import MenuList from "./components/MenuList";
 
-let menu = [{ name: "Щи" }, { name: "Шашлык" }, { name: "Чикенбургер" }];
+let menu = [{ name: "Щи", price: 240 }, { name: "Шашлык", price: 330 }, { name: "Чикенбургер", price: 200 }];
 
 function App() {
   return (
     <div className="App">
       <header></header>
-      <div className='container'>
-        <div className='columns-1 columns-sn-3'>
+      <div className='container mx-auto mt-2.5'>
+        <div className='grid gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3'>
           <MenuList items={menu} />
         </div>
       </div>
--- src/components/menuList/index.js
+++ src/components/menuList/index.js
@@ -1,5 +1,6 @@
 const MenuList = ({ items }) => {
-    return items.map((item) => <div>{item.name}</div>);
+    return items.map((item) => <div className="bg-yellow-500 border-0 "><div className="">{item.name}</div><br></br>
+        стоимость блюда: {item.price} руб.</div>);
 };
 
 export default MenuList;
