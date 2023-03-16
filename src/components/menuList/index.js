2370cc219c671fbf61ad3e4202f6b7ad2ecac432
Khaan93 <westtoraise@gmail.com>
2023-03-16 17:46:06 +0800

    18 lesson

--- src/components/menuList/index.js
+++ src/components/menuList/index.js
@@ -6,8 +6,12 @@ const MenuList = ({ items, onSelect }) => {
             className={"Playfair Display " + styles.card}
         >
             <h3 className={"bg-slate-300 border-0 opacity-100 hover:opacity-90 text-lg cursor-default "}>{item.name}</h3>
-            <img className={"opacity-60 hover:opacity-90 "} src="https://lifehacker.ru/wp-content/uploads/2015/10/Depositphotos_44100665_l-2015_1444918558-e1522781212599.jpg"
-                alt=" Картинка Щи"></img>
+            <div className={"opacity-90 hover:opacity-100 "}><img
+                src={item.link}
+                className="mx-auto my-auto "
+                alt="foodPicture"
+            />
+            </div>
             <div className={"bg-slate-100 border-0 text-sm cursor-default "}>Ингредиенты: {item.ingredients}</div>
             <div className={"bg-slate-100 border-0 text-lg cursor-default "}>{item.price} руб.</div>
             <div className={"bg-slate-300 border-0 opacity-100 hover:opacity-75 text-lg cursor-grab "}
