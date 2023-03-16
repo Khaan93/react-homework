2731ea71275bfb682e33c4f2f891c6782d965c84
Khaan93 <westtoraise@gmail.com>
2023-03-16 14:01:07 +0800

    18 lesson

--- src/components/menuList/index.js
+++ src/components/menuList/index.js
@@ -3,12 +3,14 @@ import styles from "./MenuList.module.css"
 const MenuList = ({ items, onSelect }) => {
     return items.map((item, index) => (
         <div key={`card-${index}`}
-            className={"font-serif " + styles.card}
+            className={"Playfair Display " + styles.card}
         >
-            <h3 className={"bg-violet-200 border-0 opacity-100 hover:opacity-90 text-lg cursor-default "}>{item.name}</h3>
-            <div className={"bg-violet-100 border-0 text-sm cursor-default "}>Ингредиенты: {item.ingredients}</div>
-            <div className={"bg-violet-100 border-0 text-lg cursor-default "}>{item.price} руб.</div>
-            <div className={"bg-violet-300 border-0 opacity-100 hover:opacity-75 text-lg cursor-grab "}
+            <h3 className={"bg-slate-300 border-0 opacity-100 hover:opacity-90 text-lg cursor-default "}>{item.name}</h3>
+            <img className={"opacity-60 hover:opacity-90 "} src="https://lifehacker.ru/wp-content/uploads/2015/10/Depositphotos_44100665_l-2015_1444918558-e1522781212599.jpg"
+                alt=" Картинка Щи"></img>
+            <div className={"bg-slate-100 border-0 text-sm cursor-default "}>Ингредиенты: {item.ingredients}</div>
+            <div className={"bg-slate-100 border-0 text-lg cursor-default "}>{item.price} руб.</div>
+            <div className={"bg-slate-300 border-0 opacity-100 hover:opacity-75 text-lg cursor-grab "}
                 onClick={() => onSelect(item)}>Добавить в заказ</div>
         </div >
     ));
